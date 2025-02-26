bf146027639a0870dd3a3b285bbc9f2d6a4d9a6a
Hooplez <hulahooops7@gmail.com>
2025-02-26 13:14:06 -0500

    Initial commit - about me project

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+/*
+ * To compile this Rust program:
+ * 1. Using Cargo (recommended):
+ *    - Build only:    cargo build
+ *    - Build and run: cargo run
+ *
+ * 2. Using rustc directly:
+ *    - Compile:       rustc src/main.rs
+ *    - Run:           ./main (Linux/macOS) or .\main.exe (Windows)
+ *
+ * The executable will be created in the target/debug directory when using Cargo,
+ * or in the current directory when using rustc.
+ */
+
+fn main() {
+    println!("just a test");
+
+    print!("I'm learning rust");
+    print!("I'm a rustacean");
+}
